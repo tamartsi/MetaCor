2c13ef71d6b6829b27534cba82161970e1666342
Tamar Sofer <tamartsi@users.noreply.github.com>
2018-11-21 15:42:22 -0500

    Update R/MetaCor-internal.R
    Updated to the GENESIS function, which works with sparse matrices.

--- R/MetaCor-internal.R
+++ R/MetaCor-internal.R
@@ -1,105 +1,70 @@
-.runAIREMLgaussian <-
-function(Y, W, start, m, g, n, k, covMatList, group.idx, AIREML.tol, dropZeros, maxIter, verbose){
-    
+.runAIREMLgaussian <- function(Y, X, start, covMatList, group.idx, AIREML.tol, drop.zeros, max.iter, verbose){
+
     # initial values
-    sigma2.p <- var(Y)
+    m <- length(covMatList)
+    g <- length(group.idx)
+    n <- length(Y)
+    sigma2.p <- drop(var(Y))
     AIREML.tol <- AIREML.tol*sigma2.p  # set convergence tolerance dependent on trait
+    val <- 2 * AIREML.tol
     if(is.null(start)){
         sigma2.k <- rep((1/(m+1))*sigma2.p, (m+g))
     }else{
         sigma2.k <- as.vector(start)
     }
-    
+
     reps <- 0
+    
     repeat({
         reps <- reps+1
         
         zeroFLAG <- sigma2.k < AIREML.tol # which elements have converged to "0"
         sigma2.k[zeroFLAG] <- 0 # set these to 0
         
-        # phenotype covariance matrix
-        Vre <- Reduce("+", mapply("*", covMatList, sigma2.k[1:m], SIMPLIFY=FALSE))
+        ## replace with:
+        sq <- .computeSigmaQuantities(varComp = sigma2.k, covMatList = covMatList, group.idx = group.idx)
         
-        V <- Vre
-        if(g == 1){
-            diag(V) <- diag(V) + sigma2.k[m+1]
-        }else{
-            diagV <- rep(0,n)
-            for(i in 1:g){
-                diagV[group.idx[[i]]] <- sigma2.k[m+i]
-            }
-            diag(V) <- diag(V) + diagV
-        }
-        
-        # cholesky decomposition
-        cholV <- chol(V)
-        # inverse
-        Vinv <- chol2inv(cholV)
-        VinvW <- crossprod(Vinv,W)
-        cholWtVinvW <- chol(crossprod(W, VinvW))
-        WtVinvWInv <- chol2inv(cholWtVinvW)
-        beta <- crossprod(WtVinvWInv, crossprod(VinvW,Y))
-        fits <- tcrossprod(W, t(beta))
-        residM <- as.vector(Y - fits)
-        VinvR <- crossprod(Vinv, residM)
-        RVinvR <- crossprod(residM, VinvR)
-        # residual sum of squares
-        RSS <- as.numeric(RVinvR/(n-k))
-        # log likelihood
-        logLik <- as.numeric( -0.5*n*log(2*pi*RSS) - sum(log(diag(cholV))) - 0.5*RVinvR/RSS )
-        logLikR <- as.numeric( logLik + 0.5*k*log(2*pi*RSS) - sum(log(diag(cholWtVinvW))) )
+        lq <- .calcLikelihoodQuantities(Y, X, sq$Sigma.inv, diag(sq$cholSigma), PPY=(g==1))
         
         # print current estimates
-        if(verbose) print(c(sigma2.k, logLikR, RSS))
+        if(verbose) print(c(sigma2.k, lq$logLikR, lq$RSS))
         
-        # projection matrix
-        P <- Vinv - tcrossprod(tcrossprod(VinvW,WtVinvWInv),VinvW)
+        ## check for convergence
+        if (val < AIREML.tol) {
+            converged <- TRUE
+            (break)()
+        }
         
-        # vector for later use
-        PY <- crossprod(P,Y)
+        ## check if exceeded the number of iterations
+        if (reps > max.iter) {
+            converged <- FALSE
+            warning("Maximum number of iterations reached without convergence!")
+            (break)()
+        }
+        
         
         if(reps > 1){
             # Average Information and Scores
             AI <- matrix(NA, nrow=(m+g), ncol=(m+g))
             score <- rep(NA,(m+g))
-            for(i in 1:m){
-                PAPY <- crossprod(P,crossprod(covMatList[[i]],PY))
-                score[i] <- -0.5*(sum(P*covMatList[[i]]) - crossprod(Y, PAPY)) # tr(PA) - YPAPY
-                AI[i,i] <- 0.5*crossprod(PY, crossprod(covMatList[[i]],PAPY)) # YPAPAPY
-                if((i+1) <= m){
-                    for(j in (i+1):m){
-                        AI[i,j] <- 0.5*crossprod(PY, crossprod(covMatList[[j]],PAPY)) # YPDPAPY
-                        AI[j,i] <- AI[i,j]
-                    }
-                }
-                if(g == 1){
-                    AI[i,(m+1)] <- 0.5*crossprod(PY, PAPY) # YPIPAPY
-                    AI[(m+1),i] <- AI[i,(m+1)]
-                }else{
-                    for(j in 1:g){
-                        AI[i,m+j] <- 0.5*crossprod(PY[group.idx[[j]]], PAPY[group.idx[[j]]]) # YP(I_group)PAPY
-                        AI[m+j,i] <- AI[i,m+j]
-                    }
-                }
-            }
-            if(g == 1){
-                score[m+1] <- -0.5*(sum(diag(P)) - crossprod(PY)) # tr(P) - YPIPY
-                AI[(m+1),(m+1)] <- 0.5*crossprod(PY,crossprod(P,PY)) # YPIPIPY
-            }else{
-                for(i in 1:g){
-                    PIPY <- crossprod(P[group.idx[[i]], ],PY[group.idx[[i]]])
-                    score[m+i] <- -0.5*(sum(diag(P)[group.idx[[i]]]) - crossprod(PY[group.idx[[i]]])) # tr(P(I_group)) - YP(I_group)PY
-                    AI[m+i,m+i] <- 0.5*crossprod(PY[group.idx[[i]]], PIPY[group.idx[[i]]]) # YP(I_group)P(I_group)PY
-                    if((i+1) <= g){
-                        for(j in (i+1):g){
-                            AI[m+i,m+j] <- 0.5*crossprod(PY[group.idx[[j]]], PIPY[group.idx[[j]]]) # YP(I_group2)P(I_group)PY
-                            AI[m+j,m+i] <- AI[m+i,m+j]
-                        }
-                    }
-                }
-            }
+            covMats.score.AI <- .calcAIcovMats(Y = Y,
+                                               PY = lq$PY, covMatList = covMatList,
+                                               Sigma.inv = sq$Sigma.inv, Sigma.inv_X = lq$Sigma.inv_X, Xt_Sigma.inv_X.inv = lq$Xt_Sigma.inv_X.inv)
+            AI[1:m, 1:m] <- covMats.score.AI$AI
+            score[1:m]  <- covMats.score.AI$score
+            het.vars.score.AI <- .calcAIhetvars(lq$PY, lq$PPY, group.idx,
+                                                Sigma.inv = sq$Sigma.inv, Sigma.inv_X = lq$Sigma.inv_X, Xt_Sigma.inv_X.inv = lq$Xt_Sigma.inv_X.inv)
+            score[(m + 1):(m + g)]  <- het.vars.score.AI$score
+            AI[(m + 1):(m + g),(m+1):(m + g)]  <- het.vars.score.AI$AI
             
-            if(dropZeros){
+            ### take care of "off diagonal" (terms for covariance between variance components corresponding to 
+            ### the covariance matriecs, and the residuals variances) 
+            AI.off <- .calcAIcovMatsResids(lq$PY, covMatList, group.idx,
+                                           Sigma.inv = sq$Sigma.inv, Sigma.inv_X = lq$Sigma.inv_X, Xt_Sigma.inv_X.inv = lq$Xt_Sigma.inv_X.inv)
+            AI[1:m, (m + 1):(m + g)] <- AI.off
+            AI[(m + 1):(m + g),1:m ] <- t(AI.off)
+            
+            if(drop.zeros){
                 # remove Zero terms
                 AI <- AI[!zeroFLAG,!zeroFLAG]
                 score <- score[!zeroFLAG]
@@ -108,7 +73,7 @@ function(Y, W, start, m, g, n, k, covMatList, group.idx, AIREML.tol, dropZeros, 
             # update
             AIinvScore <- solve(AI, score)
             
-            if(dropZeros){
+            if(drop.zeros){
                 sigma2.kplus1[!zeroFLAG] <- sigma2.k[!zeroFLAG] + AIinvScore
                 sigma2.kplus1[zeroFLAG] <- 0
             }else{
@@ -120,7 +85,7 @@ function(Y, W, start, m, g, n, k, covMatList, group.idx, AIREML.tol, dropZeros, 
             tau <- 1
             while(!all(sigma2.kplus1 >= 0)){
                 tau <- 0.5*tau
-                if(dropZeros){
+                if(drop.zeros){
                     sigma2.kplus1[!zeroFLAG] <- sigma2.k[!zeroFLAG] + tau*AIinvScore
                     sigma2.kplus1[zeroFLAG] <- 0
                 }else{
@@ -129,32 +94,44 @@ function(Y, W, start, m, g, n, k, covMatList, group.idx, AIREML.tol, dropZeros, 
                 }
             }
             
-            # test for convergence
-            stat <- sqrt(sum((sigma2.kplus1 - sigma2.k)^2))
+            zeroFLAG <- sigma2.kplus1 < AIREML.tol
+            sigma2.kplus1[zeroFLAG] <- 0
+            val <- sqrt(sum((sigma2.kplus1 - sigma2.k)^2))
+
             # update estimates
             sigma2.k <- sigma2.kplus1
-            if(stat < AIREML.tol){
-                converged <- TRUE
-                break()
-            }
-            if(reps == maxIter){
-                converged <- FALSE
-                warning("Maximum number of iterations reached without convergence!")
-                break()
-            }
             
         }else{
             # EM step
             sigma2.kplus1 <- rep(NA,(m+g))
             for(i in 1:m){
-                PAPY <- crossprod(P,crossprod(covMatList[[i]],PY))
-                sigma2.kplus1[i] <- (1/n)*(sigma2.k[i]^2*crossprod(Y,PAPY) + n*sigma2.k[i] - sigma2.k[i]^2*sum(P*covMatList[[i]]))
+                ### PAPY <- crossprod(lq$P,crossprod(covMatList[[i]],lq$PY))
+
+                PAPY <- sq$Sigma.inv %*% crossprod(covMatList[[i]],lq$PY) - tcrossprod(tcrossprod(lq$Sigma.inv_X, lq$Xt_Sigma.inv_X.inv), t(crossprod(covMatList[[i]],lq$PY)) %*% lq$Sigma.inv_X)
+                trPA.part1 <- sum( sq$Sigma.inv * covMatList[[i]] )
+                trPA.part2 <- sum(diag( (crossprod( lq$Sigma.inv_X, covMatList[[i]]) %*% lq$Sigma.inv_X) %*% lq$Xt_Sigma.inv_X.inv ))
+                trPA <-  trPA.part1 - trPA.part2
+                
+                ### sigma2.kplus1[i] <- (1/n)*(sigma2.k[i]^2*crossprod(Y,PAPY) + n*sigma2.k[i] - sigma2.k[i]^2*sum(lq$P*covMatList[[i]]))
+                sigma2.kplus1[i] <- as.numeric((1/n)*(sigma2.k[i]^2*crossprod(Y,PAPY) + n*sigma2.k[i] - sigma2.k[i]^2*trPA ))
+                
             }
             if(g == 1){
-                sigma2.kplus1[m+1] <- (1/n)*(sigma2.k[m+1]^2*crossprod(PY) + n*sigma2.k[m+1] - sigma2.k[m+1]^2*sum(diag(P)))
+                ### sigma2.kplus1[m+1] <- (1/n)*(sigma2.k[m+1]^2*crossprod(lq$PY) + n*sigma2.k[m+1] - sigma2.k[m+1]^2*sum(diag(lq$P)))
+                
+                trP.part1 <- sum(diag( sq$Sigma.inv ))
+                trP.part2 <- sum(diag( crossprod( lq$Sigma.inv_X) %*% lq$Xt_Sigma.inv_X.inv ))
+                trP <-  trP.part1 - trP.part2
+                
+                sigma2.kplus1[m+1] <- as.numeric((1/n)*(sigma2.k[m+1]^2*crossprod(lq$PY) + n*sigma2.k[m+1] - sigma2.k[m+1]^2*trP ))
             }else{
                 for(i in 1:g){
-                    sigma2.kplus1[m+i] <- (1/n)*(sigma2.k[m+i]^2*crossprod(PY[group.idx[[i]]]) + n*sigma2.k[m+i] - sigma2.k[m+i]^2*sum(diag(P)[group.idx[[i]]]))
+                    ### sigma2.kplus1[m+i] <- (1/n)*(sigma2.k[m+i]^2*crossprod(lq$PY[group.idx[[i]]]) + n*sigma2.k[m+i] - sigma2.k[m+i]^2*sum(diag(lq$P)[group.idx[[i]]]))
+                    covMati <- Diagonal( x=as.numeric( 1:n %in% group.idx[[i]] ) )
+                    trPi.part1 <- sum(diag(sq$Sigma.inv)[ group.idx[[i]] ] )
+                    trPi.part2 <- sum(diag( (crossprod( lq$Sigma.inv_X, covMati) %*% lq$Sigma.inv_X) %*% lq$Xt_Sigma.inv_X.inv ))
+                    trPi <- trPi.part1 - trPi.part2
+                    sigma2.kplus1[m+i] <- as.numeric((1/n)*(sigma2.k[m+i]^2*crossprod(lq$PY[group.idx[[i]]]) + n*sigma2.k[m+i] - sigma2.k[m+i]^2*trPi ))
                 }
             }
             sigma2.k <- sigma2.kplus1
@@ -163,11 +140,17 @@ function(Y, W, start, m, g, n, k, covMatList, group.idx, AIREML.tol, dropZeros, 
     })
     
     # linear predictor
-    eta <- fits + crossprod(Vre, VinvR) # X\beta + Zb
+    eta <- as.numeric(lq$fits + crossprod(sq$Vre, lq$Sigma.inv_R)) # X\beta + Zb
     
-    return(list(sigma2.k = sigma2.k, AI = AI, converged = converged, zeroFLAG = zeroFLAG, beta=beta, eta=eta, logLikR=logLikR, logLik=logLik, RSS=RSS))
+    return(list(varComp = sigma2.k, AI = AI, converged = converged, zeroFLAG = zeroFLAG, beta = lq$beta, residM = lq$residM, eta = eta, logLikR = lq$logLikR, logLik = lq$logLik, RSS = lq$RSS, fits = lq$fits))
     
 }
+
+
+
+
+
+
 .subsetCholSigmaInv <-
 function(cholSigmaInv, chol.idx) {
     if(length(chol.idx) > 0){
